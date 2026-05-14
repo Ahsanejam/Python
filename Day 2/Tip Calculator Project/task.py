eae51158a1626dcb59b374a8a1e7515cb07bceff
Ahsan Ejam <ahsanejam48@gmail.com>
2026-05-14 19:42:52 +0530

    Day 2 file uploaded

---
+++ Day 2/Tip Calculator Project/task.py
@@ -0,0 +1,26 @@
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+# people = int(input("How many people to split the bill? "))
+
+
+print("What was the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, 15? "))
+people = int(input("How many people to spilt the bill? "))
+
+value = (tip/100) * bill
+bill2 = (value + bill)
+div = (bill2 / people)
+print(f"Each person should pay: ${round(div, 2)}")
+
+
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+# print(f"Each person should pay: ${final_amount}")
+
+
+
---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,18 @@
+# len("Hello")
+#
+# print(type("Hello"))
+#
+# print(type("Hello"))
+# print(type(123))
+# print(type(3.14))
+# print(type(True))
+
+
+# Type Casting or Conversion
+print(int("123") + int("456"))
+
+name_of_the_user = input("Enter your name")
+length_of_name = len(name_of_the_user)
+
+
+print("Number of letters in your name: " + str(length_of_name))
