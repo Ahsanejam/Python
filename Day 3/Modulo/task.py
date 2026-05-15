4b9c0ddeddca04504ef848854914795ce3213513
Ahsan Ejam <ahsanejam48@gmail.com>
2026-05-15 19:00:00 +0530

    Add new file

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,8 @@
+# print(10 % 3)
+
+# Ever or odd
+number_to_check = int(input("What is a number you want to check? "))
+if number_to_check % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ Day 3/Python Pizza/task.py
@@ -0,0 +1,44 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+# My Method method 1
+# if size == "S":
+#     bill += 15
+#     if pepperoni == "Y":
+#         bill += 2
+# elif size == "M":
+#     bill += 20
+#     if pepperoni == "Y":
+#         bill += 3
+# else:
+#     bill += 25
+#     if pepperoni == "Y":
+#         bill += 3
+# if extra_cheese == "Y":
+#     bill += 1
+# print(f"Your final bill is ${bill}")
+
+
+# Method - 2
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("You enter wrong inputs.")
+
+#  Pepperoni
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+#Final project
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
